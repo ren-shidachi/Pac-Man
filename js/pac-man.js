6d1d29cc968034e621b045f8b40f913e22440978
ren-shidachi <togetukotaro@gmail.com>
2016-10-19 20:22:41 +0900

    working on rendering background

--- js/pac-man.js
+++ js/pac-man.js
@@ -89,9 +89,48 @@ document.onkeyup = function(e) {
  * canvas and crates a black image.
  */
 function Background() {
-    this.draw = function() {
+    this.draw = function(map) {
         this.context.fillStyle = 'black';
         this.context.fillRect(this.x, this.y, this.canvasWidth, this.canvasHeight);
+        for (n=0; n<map.length; n++){
+            for (i=0; i<map[n].length; i++){
+                console.log(i);
+            }
+        }
     };
 }
 Background.prototype = new Drawable();
+
+/**
+ * This is the game object that takes care of all the objects 
+ * runned in side the game
+ */
+function Game(){
+    this.init = function () {
+        this.bgCanvas = $('#background')[0];
+        if (this.bgCanvas.getContext) { 
+            this.bgContext = this.bgCanvas.getContext('2d');
+            // Initialize objects, adding context and canvas information
+            Background.prototype.context = this.bgContext;
+            Background.prototype.canvasWidth = this.bgCanvas.width;
+            Background.prototype.canvasHeight = this.bgCanvas.height;
+            // Initialize background
+            this.background = new Background();
+            this.background.init(0,0);
+            var map = [
+                [1,0,0],
+                [0,0,0],
+                [0,1,1]
+            ];
+            this.background.draw(map);
+        }
+    };
+}
+
+$(function () {
+    $("#game-start").click(function () {
+        var game = new Game();
+        console.log('Started');
+        game.init();
+    });
+});
