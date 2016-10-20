934598e264bad16d5c98de1947583e1ad33cbb9d
ren-shidachi <togetukotaro@gmail.com>
2016-10-20 16:09:46 +0900

    introduced zoomFactor

--- js/pac-man.js
+++ js/pac-man.js
@@ -4,6 +4,11 @@
  * singleton.
  */
 var imageRepository = new function(){
+    // Define images
+    this.parts = new Image();
+
+    // Set images src
+    this.parts.src = "img/common.png";
 }
 
 /**
@@ -89,17 +94,34 @@ document.onkeyup = function(e) {
  * canvas and crates a black image.
  */
 function Background() {
-    this.draw = function(map) {
+    this.draw = function(map, z) {
         this.context.fillStyle = 'black';
         this.context.fillRect(this.x, this.y, this.canvasWidth, this.canvasHeight);
         for (n=0; n<map.length; n++){
             for (i=0; i<map[n].length; i++){
-                console.log(i);
+                if(map[n][i] > 1){
+                    x = map[n][i];
+                    this.context.drawImage(imageRepository.parts, 8*(x-1), 0, 8, 8, 8*i*z, 8*n*z, 8*z, 8*z);
+                }
             }
         }
     };
 }
 Background.prototype = new Drawable();
+
+function PacMan() {
+    this.init = function(x, y){
+        this.x = x;
+        this.y = y;
+        this.width = 8;
+        this.height = 8;
+        this.isColliding = false;
+    };
+    this.draw = function (z){
+        this.context.drawImage(imageRepository.parts, 0, 8, 13, 13, (8*(this.x)-6)*z, (8*this.y-3)*z, 13*z,13*z);
+    };
+}
+PacMan.prototype = new Drawable();
 
 /**
  * This is the game object that takes care of all the objects 
@@ -107,30 +129,77 @@ Background.prototype = new Drawable();
  */
 function Game(){
     this.init = function () {
-        this.bgCanvas = $('#background')[0];
+        this.bgCanvas = $("#background")[0];
+        this.mainCanvas = $("#main")[0];
         if (this.bgCanvas.getContext) { 
             this.bgContext = this.bgCanvas.getContext('2d');
+            this.mainContext = this.mainCanvas.getContext('2d');
             // Initialize objects, adding context and canvas information
             Background.prototype.context = this.bgContext;
             Background.prototype.canvasWidth = this.bgCanvas.width;
             Background.prototype.canvasHeight = this.bgCanvas.height;
+            PacMan.prototype.context = this.mainContext;
+            PacMan.prototype.canvasWidth = this.mainCanvas.width;
+            PacMan.prototype.canvasHeight = this.mainCanvas.height;
             // Initialize background
             this.background = new Background();
             this.background.init(0,0);
-            var map = [
-                [1,0,0],
-                [0,0,0],
-                [0,1,1]
-            ];
-            this.background.draw(map);
+            // Initialize pacman
+            this.pacman = new PacMan();
+            this.pacman.init(3,6);
+
+            return true;
         }
     };
+    this.start = function (zoomFactor) {
+        var map = [
+            [6,9,9,9,9,3],
+            [10,0,0,0,0,8],
+            [10,0,6,3,0,8],
+            [10,0,2,2,0,8],
+            [10,0,2,2,0,8],
+            [10,0,5,4,0,8],
+            [10,0,0,0,0,8],
+            [5,7,7,7,7,4],
+        ];
+        this.background.draw(map, zoomFactor);
+        this.pacman.draw(zoomFactor);
+    }
 }
 
+
+/**
+ * requestAnim shim layer by Paul Irish
+ * Finds the first API that works to optimize the animation loop,
+ * otherwize defaults to setTimeout().
+ */
+window.requestAnimFrame = (function(){
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(/* function */ callback, /* DOMElement */ element){
+            window.setTimeout(callback, 1000/ 60);
+        };
+})();
+
+
+// main
 $(function () {
+    // Resize game area
+    var zoomFactor = 4;
+    var width = $("#main-area").width() * zoomFactor;
+    var height = $("#main-area").height() * zoomFactor;
+    $("#main-area").width(width).height(height);
+    $("#main-area canvas").each(function () {
+        this.width = width;
+        this.height = height;
+    });
     $("#game-start").click(function () {
         var game = new Game();
-        console.log('Started');
-        game.init();
+        if (game.init()) {
+            game.start(zoomFactor);
+        }
     });
 });
